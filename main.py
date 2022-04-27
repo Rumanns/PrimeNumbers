406c6c2a5e992aaa71a145453a069e59e4eba8ae
José Valdeir <43117843+Rumanns@users.noreply.github.com>
2022-04-27 09:48:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,34 +3,8 @@
 # Date: 05/01/2021
 # Author: José Valdeir
 
-"""primos = [2]
-pos = 0
-n = 2
-while n < 100:
-    n += 1
-    primos.append(n)
-    if n % primos[pos]:
-        print(primos)
-"""
-'''    if n % primos[n] != 0:
-        primos.append(n)
-    print(primos)
-'''
-
-
-'''    while n % primos[pos] != 0:
-        c -= 1
-    else:
-        if c == 1:
-            print(n, 'é primo!')
-        else:
-            print(n, 'não é primo')
-    pos += 1
-'''
-
-
-
-#Programa 1 - Totalmente bem sucedido! Mas lento - Acima estão as tentativas de fazer um mais eficiente.
+#Programa 1 - Totalmente bem sucedido! Mas lento porque divide por cada número a partir de 2.
+#E no momento estou desenvolvendo outro mais eficiente que faça a divisão de cada número de um intervalor apenas pelos números primos e não por cada número até o valor máximo.
 #Maior número primo existente: (2**(82.589.933))-1
 n = 10000001
 while 10000000 < n < 10000100:
